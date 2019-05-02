0d36c72162d1dad75af5956e230a6791c0e8db47
gpedro <gpedro831@gmail.com>
2019-05-01 22:19:51 -0400

    fix sql injection (resolves #4)

--- pages/tankyou.php
+++ pages/tankyou.php
@@ -21,7 +21,9 @@ if (DEBUG_DATABASE)
 
 if(isset($_REQUEST['tcode'])){
     $tcode = $_REQUEST['tcode'];
-    $code_status = $SQL->query("SELECT * FROM `pagseguro_transactions` WHERE `transaction_code` = '{$tcode}'")->fetchAll();
+    $code_query = $SQL->prepare("SELECT * FROM `pagseguro_transactions` WHERE `transaction_code` = :tcode");
+    $code_query->execute(['tcode' => $tcode]);
+    $code_status = $code_query->fetchAll();
     $count = count($code_status);
     $code_status = $code_status[0];
     if($count > 0){
